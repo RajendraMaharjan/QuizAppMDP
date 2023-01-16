d3aadd2a5a7d0cf23c63bf51fd95c80ef2096ea2
Rajendra Maharjan <3464136+RajendraMaharjan@users.noreply.github.com>
2023-01-15 21:06:06 -0500

    fragment and result analysis implementation

--- app/src/main/java/com/rajendra/quizapp/model/QuizDemoData.kt
+++ app/src/main/java/com/rajendra/quizapp/model/QuizDemoData.kt
@@ -10,168 +10,222 @@ class QuizDemoData {
             return mutableListOf(
                 Quiz(
                     1,
-                    "1. Android is _____",
+                    "Q 1 - What is Pending Intent in android?",
                     IS_RADIO_BUTTON,
-                    "Android is a software package and linux based operating system for mobile devices such as tablet computers and smartphones.",
-                    "a",
-                    "an operating system",
+                    "Pending Intent is fired or triggered at a future point of time.",
+                    "c",
+                    "It will fire at a future point of time.",
                     "",
                     "",
-                    mutableListOf("an operating system","a web browser", "a web server", "None of the above")
+                    mutableListOf(
+                        "It is a kind of an intent.",
+                        "It is used to pass the data between activities.",
+                        "It will fire at a future point of time.",
+                        "None of the Above."
+                    )
                 ),
                 Quiz(
                     2,
-                    "2. Under which of the following Android is licensed?",
+                    "Q 2 - What is the life cycle of foreground activity in android?",
                     IS_RADIO_BUTTON,
-                    "The Android platform was released under the Apache 2.0 license, and it is responsible for the copyright of the Android Open Source project.",
+                    "Foreground activity should be onCreate() −> onStart() −> onResume() in activity life cycle.",
                     "c",
-                    "Apache/MIT",
+                    "onCreate() −> onStart() −> onResume()",
                     "",
                     "",
-                    mutableListOf("OSS","Sourceforge","Apache/MIT","None of the above")
+                    mutableListOf(
+                        "onCreate() −> onStart() −> onResume() −> onStop() −> onRestart",
+                        "onCreate() −> onStart() −> onResume() −>onStop()",
+                        "onCreate() −> onStart() −> onResume()",
+                        "None of the above"
+                    )
                 ),
                 Quiz(
                     3,
-                    "3. For which of the following Android is mainly developed?",
+                    "Q 3 - What is fragment in android?",
                     IS_RADIO_BUTTON,
-                    "Android is a software package and a Linux-based operating system specially designed for touch-screen mobile devices like smartphones and tablets.",
-                    "d",
-                    "Mobile devices",
+                    "Fragment is a piece of an activity. If you want to move your application in 360 degrees, you can do this by using a fragment.",
+                    "b",
+                    "Piece of Activity",
                     "",
                     "",
-                    mutableListOf("Servers","Desktops","Laptops","Mobile devices")
+                    mutableListOf("JSON", "Piece of Activity", "Layout", "None of Above")
                 ),
                 Quiz(
                     4,
-                    "4. Which of the following virtual machine is used by the Android operating system?",
+                    "Q 4 - Is it possible activity without UI in android?",
                     IS_RADIO_BUTTON,
-                    "The Dalvik Virtual Machine (DVM) is an android virtual machine optimized for mobile devices. It optimizes the virtual machine for memory, battery life, and performance.",
+                    "Without UI, we can call an activity, It will do some background functionalities.",
                     "b",
-                    "Dalvik virtual machine",
+                    "Yes,it's possible",
                     "",
                     "",
-                    mutableListOf("JVM","Dalvik virtual machine","Simple virtual machine","None of the above")
+                    mutableListOf(
+                        "No, it's not possible",
+                        "Yes,it's possible",
+                        "We can't say",
+                        "None of the above"
+                    )
                 ),
                 Quiz(
                     5,
-                    "5. Android is based on which of the following language?",
+                    "Q 5 - How to get current location in android?",
                     IS_RADIO_BUTTON,
-                    "Java language is mainly used to write the android code even though other languages can be used.",
-                    "a",
-                    "Java",
+                    "GPRS and Location provider is used to fetch the current location of a phone as longitude and latitude.",
+                    "c",
+                    "A & B",
                     "",
                     "",
-                    mutableListOf("Java","C++","Python","C")
+                    mutableListOf(
+                        "Using with GPRS",
+                        "Using location provider",
+                        "A & B",
+                        "SQlite",
+                        "Network servers"
+                    )
                 ),
                 Quiz(
                     6,
-                    "6. APK stands for _____",
+                    "Q 6 - How many applications are there in a given task in android?",
                     IS_RADIO_BUTTON,
-                    "An APK is a short form of the Android Package Kit. An APK file is the file format used to install the applications on the android operating system.",
+                    "The task is a collaboration of applications. It contains one or more applications in it.",
                     "c",
-                    "Android Package Kit",
+                    "Many",
                     "",
                     "",
-                    mutableListOf("Android Phone Kit","Android Page Kit","Android Package Kit")
+                    mutableListOf(
+                        "Two",
+                        "One",
+                        "Many",
+                        "Zero"
+                    )
                 ),
                 Quiz(
                     7,
-                    "7. What does API stand for?",
+                    "Q 7 - What does API stand for?",
                     IS_RADIO_BUTTON,
                     "API stands for application program interface. It is a set of routines, protocols, and tools for building software and applications.",
                     "a",
                     "Application Programming Interface",
                     "",
                     "",
-                    mutableListOf("Application Programming Interface","Android Programming Interface","Android Page Interface", "Application Page Interface")
+                    mutableListOf(
+                        "Application Programming Interface",
+                        "Android Programming Interface",
+                        "Android Page Interface",
+                        "Application Page Interface"
+                    )
                 ),
                 Quiz(
                     8,
-                    "8. How can we kill an activity in android?",
+                    "Q 8 - How can we kill an activity in android?",
                     IS_CHECK_BOX,
                     "The finish() method is used to close the activity. Whereas the finishActivity(int requestCode) also closes the activity with requestCode.",
                     "ab",
                     "Using finish() method\nUsing finishActivity(int requestCode)",
                     "",
                     "",
-                    mutableListOf("Using finish() method","Using finishActivity(int requestCode)","Neither (a) nor (b)")
+                    mutableListOf(
+                        "Using finish() method",
+                        "Using finishActivity(int requestCode)",
+                        "Neither (a) nor (b)"
+                    )
                 ),
                 Quiz(
                     9,
-                    "9. On which of the following, developers can test the application, during developing the android applications?",
+                    "Q 9 - What does httpclient.execute() returns in android?",
                     IS_CHECK_BOX,
-                    "We can use the Android emulator, physical android phone, or third-party emulator as a target device to execute and test our Android application.",
-                    "abc",
-                    "Third-party emulators\nEmulator included in Android SDK\nPhysical android phone",
+                    "Httpclient.execute() executes only once and it will return http response from the server or device, Http entity is embedded the body of the Http response.",
+                    "b",
+                    "Http response",
                     "",
                     "",
-                    mutableListOf("Third-party emulators","Emulator included in Android SDK","Physical android phone")
+                    mutableListOf(
+                        "Http entity",
+                        "Http response",
+                        "Http result",
+                        "Http Client"
+                    )
                 ),
                 Quiz(
                     10,
-                    "10. Which of the following is not an activity lifecycle callback method?",
+                    "Q 10 - Which of the following is not an activity lifecycle callback method?",
                     IS_RADIO_BUTTON,
                     "onBackPressed() method is not an activity lifecycle callback method",
                     "d",
                     "onBackPressed() method",
                     "",
                     "",
-                    mutableListOf("onClick() method","onCreate() method","onStart() method","onBackPressed() method")
+                    mutableListOf(
+                        "onClick() method",
+                        "onCreate() method",
+                        "onStart() method",
+                        "onBackPressed() method"
+                    )
                 ),
                 Quiz(
                     11,
-                    "11. Which of the following is a dialog class in android?",
+                    "Q 11 - Which of the following is a dialog class in android?",
                     IS_CHECK_BOX,
                     "All of the above are Dialog classes",
                     "abc",
                     "AlertDialog\nDatePicker Dialog\nProgressDialog",
                     "",
                     "",
-                    mutableListOf("AlertDialog","DatePicker Dialog","ProgressDialog")
+                    mutableListOf("AlertDialog", "DatePicker Dialog", "ProgressDialog")
                 ),
                 Quiz(
                     12,
-                    "12. Which of the following android component displays the part of an activity on screen?",
+                    "Q 12 - Which of the following android component displays the part of an activity on screen?",
                     IS_RADIO_BUTTON,
                     "Android Fragment is the part of activity; it is also known as sub-activity. There can be more than one fragment in an activity. Fragments represent multiple screens inside one activity.",
                     "d",
                     "Fragment",
                     "",
                     "",
-                    mutableListOf("View","Manifest","Gradle","Fragment")
+                    mutableListOf("View", "Manifest", "Gradle", "Fragment")
                 ),
                 Quiz(
                     13,
-                    "13. Which of the following is the parent class of Activity?",
+                    "Q 13 - Which of the following is the parent class of Activity?",
                     IS_RADIO_BUTTON,
                     "The android Activity is the subclass of ContextThemeWrapper class. Android Activity Lifecycle is controlled by 7 methods of android.app.Activity class. An activity is a single screen in android. It is like a window or frame of Java.",
                     "c",
                     "contextThemeWrapper",
                     "",
                     "",
-                    mutableListOf("context","object","contextThemeWrapper","None of the above")
+                    mutableListOf("context", "object", "contextThemeWrapper", "None of the above")
                 ),
                 Quiz(
                     14,
-                    "14. Which of the following features are updated in Android 4.1(Jelly Bean)?",
+                    "Q 14 - Which of the following features are updated in Android 4.1(Jelly Bean)?",
                     IS_CHECK_BOX,
                     "Android 4.1(Jelly Bean) updates to following features: smoother user interface, enhance accessibility, expandable notification, fixed bug on Nexus 7, one-finger gestures to expand/collapse notifications, lock screen improvement, multiple user accounts (tablets only), new clock application, Bluetooth low energy support, volume for incoming call, 4K resolution support, native emoji support, bug fixes for the Nexus 7 LTE.",
                     "abc",
                     "User Interface\nLock screen improvement\nNew clock application",
                     "",
                     "",
-                    mutableListOf("User Interface","Lock screen improvement","New clock application")
+                    mutableListOf(
+                        "User Interface",
+                        "Lock screen improvement",
+                        "New clock application"
+                    )
                 ),
                 Quiz(
                     15,
-                    "15. Which of the following method is used by services to clean up any services?",
+                    "Q 15 - Which of the following method is used by services to clean up any services?",
                     IS_RADIO_BUTTON,
                     "The onDestroy() method can be used by services to clean up any resources like receivers, threads, registered listeners, etc.",
                     "c",
                     "onDestroy() method",
                     "",
                     "",
-                    mutableListOf("onStop() method","stopService() method","onDestroy() method","stopSelf() method")
+                    mutableListOf(
+                        "onStop() method",
+                        "stopService() method",
+                        "onDestroy() method",
+                        "stopSelf() method"
+                    )
                 )
             )
         }
